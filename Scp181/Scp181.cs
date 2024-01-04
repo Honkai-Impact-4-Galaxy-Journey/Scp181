35ca2256d5dde18b69660369e11f293515e50c10
Tiphereth <atmlz32@163.com>
2024-01-04 22:59:15 +0800

    添加项目文件。

---
+++ Scp181/Scp181.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MEC;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+
+namespace Scp181
+{
+    public class Scp181
+    {
+        public string userid;
+        [PluginEntryPoint("Scp181","1.0.0","Scp181","SIlver Wolf")]
+        public void OnEnabled()
+        {
+            Log.Info("Plugin Loaded");
+        }
+        [PluginEvent(PluginAPI.Enums.ServerEventType.RoundStart)]
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            Timing.CallDelayed(2, () => { userid = Player.GetPlayers().RandomItem().UserId; Init181(); } );
+        }
+        public void Init181()
+        {
+            Player player = Player.Get(userid);
+            player.SendBroadcast("<size=24>你是181</size>", 5, shouldClearPrevious: true);
+
+        }
+    }
+}
